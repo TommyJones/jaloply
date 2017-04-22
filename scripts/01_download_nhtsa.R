6ec3f7524f9a42fa65473670372a283ace22d8e5
Tommy Jones <jones.thos.w@gmail.com>
2017-04-22 10:20:11 -0400

    first successful data import

--- scripts/01_download_nhtsa.R
+++ scripts/01_download_nhtsa.R
@@ -71,26 +71,71 @@ ImportFlatFile <- function(filepath){
   d
 }
 
+FormatNames <- function(filepath){
+  d <- scan(filepath, what = "character", sep = "\n")
+  d <- grep(pattern = "^\\d{1} |^\\d{2} ", d, value = T)
+  d <- strsplit(d, split = " ")
+  d <- lapply(d, function(x) x[ x != "" ])
+  
+  names <- tolower(sapply(d, function(x) x[ 2 ]))
+  
+  dictionary <- lapply(d, function(x) c(x[ 1:3 ], paste(x[ 4:length(x) ], collapse = " ")))
+  
+  dictionary <- do.call(rbind, dictionary)
+  
+  colnames(dictionary) <- c("id", "name", "source_data_type", "description")
+  
+  dictionary <- as.data.frame(dictionary, stringsAsFactors = FALSE)
+  
+  list(names = names,
+       dictionary = dictionary)
+  
+}
+
 # complaints
 cmpl <- ImportFlatFile("data_raw/FLAT_CMPL.txt")
 
-cmplnames <- scan(paste0("data_raw/cmpl_", d, ".txt"), 
-                  what = "character",
-                  sep = "\n")
+cmpl_info <- FormatNames(paste0("data_raw/cmpl_", d, ".txt"))
 
-cmplnames <- grep(pattern = "^\\d{1} |^\\d{2} ", cmplnames, value = T)
+names(cmpl) <- cmpl_info[[ 1 ]]
 
-cmplnames <- strsplit(cmplnames, split = " ")
+save(cmpl, cmpl_info, file = "data_derived/cmpl_raw.RData")
 
-names(cmpl) <- sapply(cmplnames, function(x){
-  x <- x[ x != "" ]
-  x[ 2 ]
-})
-
-cmplnames <- sapply(cmplnames, function(x) paste(x[ x != "" ], collapse = " "))
+rm(cmpl, cmpl_info)
+gc()
 
 # recalls
+rcl <- ImportFlatFile("data_raw/FLAT_RCL.txt")
+
+rcl_info <- FormatNames(paste0("data_raw/rcl_", d, ".txt"))
+
+names(rcl) <- rcl_info[[ 1 ]]
+
+save(rcl, rcl_info, file = "data_derived/rcl_raw.RData")
+
+rm(rcl, rcl_info)
+gc()
 
 # investigations
+inv <- ImportFlatFile("data_raw/flat_inv.txt")
+
+inv_info <- FormatNames(paste0("data_raw/inv_", d, ".txt"))
+
+names(inv) <- inv_info[[ 1 ]]
+
+save(inv, inv_info, file = "data_derived/inv_raw.RData")
+
+rm(inv, inv_info)
+gc()
 
 # TSBs
+tsbs <- ImportFlatFile("data_raw/flat_tsbs.txt")
+
+tsbs_info <- FormatNames(paste0("data_raw/tsbs_", d, ".txt"))
+
+names(tsbs) <- tsbs_info[[ 1 ]]
+
+save(tsbs, tsbs_info, file = "data_derived/tsbs_raw.RData")
+
+rm(tsbs, tsbs_info)
+gc()
