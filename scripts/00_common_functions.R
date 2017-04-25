c214831a7420e5cb3798401628b6aa9bf0ef0986
Tommy Jones <jones.thos.w@gmail.com>
2017-04-24 20:07:27 -0400

    uncomment a function

--- scripts/00_common_functions.R
+++ scripts/00_common_functions.R
@@ -10,18 +10,19 @@ library(httr)
 
 ### Functions go here. Put a description in the comments -----------------------
 
-# recursive_rbind <- function(list_object){
-#   # this function takes a list and performs rbind in a recursive way
-#   # recursive makes it faster and more memory efficient
-#   
-#   if(length(list_object) <= 1000)
-#     return(do.call(rbind, list_object))
-#   
-#   batches <- seq(1, length(list_object), by = 1000)
-#   
-#   list_object <- lapply(batches, function(x){
-#     do.call(rbind, list_object[ x:(min(x + 999, length(list_object))) ])
-#   })
-#   
-#   recursive_rbind(list_object)
-# }
+recursive_rbind <- function(list_object){
+  # this function takes a list and performs rbind in a recursive way
+  # recursive makes it faster and more memory efficient
+
+  if(length(list_object) <= 1000)
+    return(do.call(rbind, list_object))
+
+  batches <- seq(1, length(list_object), by = 1000)
+
+  list_object <- lapply(batches, function(x){
+    do.call(rbind, list_object[ x:(min(x + 999, length(list_object))) ])
+  })
+
+  recursive_rbind(list_object)
+}
+
