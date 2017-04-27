2ef5292543eb28a61b74e6096a57ad99e58ff312
Tommy Jones <jones.thos.w@gmail.com>
2017-04-26 20:45:48 -0400

    playing in the sandbox

--- scripts/sandbox/tstat.R
+++ scripts/sandbox/tstat.R
@@ -1,146 +1,35 @@
 
+
 rm(list = ls())
 
 source("scripts/00_common_functions.R")
 
-load("data_derived/tsbs_raw.RData")
+load("data_derived/cmpl_raw.RData")
 
-d <- tsbs[ sample(1:nrow(tsbs), 5000) , c("buldte", "maketxt", "compname") ]
+data <- cmpl[ , c("datea", "maketxt", "modeltxt", "compdesc") ]
 
-str(d)
+data$datea <- as.Date(data$datea, format = "%Y%m%d")
 
-d$buldte <- as.Date(d$buldte, format = "%Y%m%d")
+data$month <- lubridate::ceiling_date(as.Date(format(data$datea, "%Y-%m-02")), "month") - 1
 
-d$month <- lubridate::ceiling_date(as.Date(format(d$buldte, "%Y-%m-02")), "month") - 1
+data$component <- sapply(strsplit(data$compdesc, split = ":"), function(x) x[ 1 ])
 
-d <- d[ format(d$month, "%Y") %in% 1970:as.numeric(format(Sys.Date(), "%Y")) &
-          ! is.na(d$month) , ]
+data$platform <- toupper(stringr::str_conv(paste(data$maketxt, data$modeltxt), "UTF-8"))
 
-date_fill <- seq(min(d$month), max(d$month), by = "day")
+date_fill <- seq(min(data$month, na.rm = T), max(data$month, na.rm = T), by = "day")
 
 date_fill <- unique(lubridate::ceiling_date(as.Date(format(date_fill, "%Y-%m-02")), "month") - 1)
 
-d <- merge(d, data.frame(month = date_fill, stringsAsFactors = F),
+data <- merge(data, data.frame(month = date_fill, stringsAsFactors = F),
            all = TRUE)
 
+counts <- CalcRollingCount(data = data[ nchar(data$platform) < 50 , ], date_var = "month", platform_var = "platform",
+                           component_var = "component")
 
-CalcRollingCount <- function(data, date_var, platform_var, component_var,
-                             window = 365, cpus = 4){
-  
-  # Subset the data on only the columns we need
-  data <- data[ , c(date_var, platform_var, component_var) ]
-  names(data) <- c("date", "platform", "component")
-  
-  data$count <- "count" # set up a counting variable
-  
-  data <- reshape2::dcast(data, date + platform + component ~ count,
-                          value.var = "count",
-                          fun.aggregate = length)
-  
-  # Do some basic cleanup to prevent errors and such
-  data$platform[ is.na(data$platform) ] <- "unknown"
-  data$component[ is.na(data$component) ] <- "unknown"
-  data <- data[ ! is.na(data$date) , ]
-  
-  # split by platform and component for first round of counts
-  data <- by(data, INDICES = data$platform, function(x) x)
-  
-  data <- parallel::mclapply(data, function(y){
-    
-    components <- by(y, INDICES = y$component, function(x){
-      
-      drange <- sort(unique(x$date))
-      
-      result <- sapply(drange, function(date){
-        sum(x$count[ x$date %in% seq(date - window - 1, date, by = "day") ], na.rm = T)
-      })
-      
-      result <- data.frame(date = drange,
-                           platform = rep(x$platform[ 1 ], length(drange)),
-                           component = rep(x$component[ 1 ], length(drange)),
-                           count = result,
-                           stringsAsFactors = F)
-      result
-    })
-    components <- recursive_rbind(components)
-    
-    components
-  }, mc.cores = cpus)
-  
-  data <- recursive_rbind(data)
-  
-  # Get platform totals
-  platforms <- reshape2::dcast(data, date ~ platform, 
-                               value.var = "count",
-                               fun.aggregate = function(x) sum(x, na.rm = T))
-  
-  platforms <- reshape2::melt(platforms, id.vars = c("date"),
-                              variable.name = "platform", 
-                              value.name = "platform_tot")
-  
-  platforms <- platforms[ platforms$platform_tot > 0 , ]
-  
-  platforms$platform <- as.character(platforms$platform)
+counts$t_stat <- CalcTStatistic(n1 = counts$count, N1 = counts$platform_tot,
+                                n2 = counts$component_tot, N2 = counts$total,
+                                scale = 800)
 
-  # Get component totals
-  components <- reshape2::dcast(data, date ~ component, 
-                               value.var = "count",
-                               fun.aggregate = function(x) sum(x, na.rm = T))
-  
-  components <- reshape2::melt(components, id.vars = c("date"),
-                              variable.name = "component", 
-                              value.name = "component_tot")
-  
-  components <- components[ components$component_tot > 0 , ]
-  
-  components$component <- as.character(components$component) 
-  
-  # Get overall totals
-  total <- reshape2::dcast(data, date ~ ., value.var = "count", 
-                           fun.aggregate = function(x) sum(x, na.rm = T))
-  names(total)[ 2 ] <- "total"
-  
-  # merge it all together
-  result <- merge(data, platforms, all = T)
-  result <- merge(result, components, all = T)
-  result <- merge(result, total, all = T)
-  
-  # final cleanup and return result
-  result <- result[ , c("date", "platform", "component", "count",
-                        "platform_tot", "component_tot", "total") ]
-  
-  result
-  
-}
-
-
-CalcTStatistic <- function(n1, N1, n2, N2, scale = NULL, cohend = FALSE){
-  
-  p1 <- n1 / N1
-  p2 <- n2 / N2
-  
-  s2_1 <- p1 * (1 - p1)
-  s2_2 <- p2 * (1 - p2)
-  
-  if (! is.null(scale) & cohend) 
-    warning("scale is not NULL and cohend is TRUE. 
-            This will create a scaled version of Cohen's d statistic 
-            (which won't change your results much). 
-            Did you mean to do this?")
-  
-  if (! is.null(scale)){
-    if (! is.numeric(scale))
-      stop("If scale is non-NULL, it must be numeric and preferrably an integer.")
-    
-    scale_f <- N1 / N2
-    N2[ N2 > scale ] <- scale
-    N1 <- scale_f * scale
-  }
-  
-  if (cohend) {
-    t <- (p1 - p2) / sqrt(((N1 - 1) * s2_1 + (N2 - 1) * s2_2) / (N1 + N2))
-  } else {
-    t <- (p1 - p2) / sqrt(s2_1 / N1 + s2_2 / N2)
-  }
-  t
-}
+counts$d_stat <- CalcTStatistic(n1 = counts$count, N1 = counts$platform_tot,
+                                n2 = counts$component_tot, N2 = counts$total,
+                                cohend = TRUE)
